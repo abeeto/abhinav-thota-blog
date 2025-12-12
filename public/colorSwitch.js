c0dc03fe2dfe3c71fe0194c76cba92c28515aac4
abeeto <59943893+abeeto@users.noreply.github.com>
2025-12-12 23:39:49 +0000

    feat: add colorswitch

--- public/colorSwitch.js
+++ public/colorSwitch.js
@@ -3,7 +3,6 @@ const metaColorScheme = document.querySelector('meta[name="color-scheme"]');
 const colorToggle = document.querySelector("#color-switch-toggle");
 
 colorToggle.addEventListener("click",  (e) => {
-    console.log("CLICKED!");
     const currentColorScheme = html.getAttribute("data-theme") || "light";
     const newColorScheme = currentColorScheme === "light" ? "dark" : "light";
     html.setAttribute("data-theme", newColorScheme);
